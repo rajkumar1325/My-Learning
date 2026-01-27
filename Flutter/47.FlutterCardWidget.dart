f80f26a2c6f51aca200b295c7c0677e440c726e7
Raj kumar <rajkumar.rk0104@gmail.com>
2026-01-27 21:54:15 +0530

    Add FlutterCardWidget with elevated card effect
    This Flutter application demonstrates a card widget with an elevated effect and customizable shadow color.

---
+++ Flutter/47.FlutterCardWidget.dart
@@ -0,0 +1,76 @@
+// Adds card like effect --> elevated effect on the  container
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+
+
+      // CARD-WIDGET
+
+      body: Card(
+        elevation: 34, // manages elevation, down the container
+        shadowColor: Colors.red,  //changes color of the shadoe
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("This is my heading!", style: TextStyle(fontSize: 34),),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
