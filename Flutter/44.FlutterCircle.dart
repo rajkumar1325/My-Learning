b62b16c064bf1dc9eb2987e17cddd7e0d9910dbf
Raj kumar <rajkumar.rk0104@gmail.com>
2026-01-27 14:07:05 +0530

    Create Flutter app with circular avatar UI
    This Flutter application displays a user profile in a circular avatar with a nested circular avatar for the user's image.

---
+++ Flutter/44.FlutterCircle.dart
@@ -0,0 +1,98 @@
+// It is used to Circle the widget
+
+import 'package:flutter/material.dart';
+
+//main function --> runner
+void main() {
+  runApp(const MyApp());
+}
+
+// here we dont have to manage state. --> cant change content in runtime as of stateless
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, //remove top-right side strip
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: .fromSeed(seedColor: Colors.deepOrangeAccent),
+      ),
+      home: const MyHomePage(
+        title: 'Hello World',
+      ), //primary page to show when app starts
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+// screen app UI
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var ArrNames = [
+      "Ram",
+      "Ravan",
+      "Ravana",
+      "Rashmi",
+      "Kaushik",
+    ]; //Array to pass
+
+    // screen main app --> returns
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+
+      body: CircleAvatar(
+        radius: 80,
+        backgroundColor: Colors.red,
+
+        // Used when we have to place widget over the Avatar
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, //vertical center
+
+              children: [
+                CircleAvatar(
+                  radius: 60,
+                  backgroundColor: Colors.red.shade200,
+                  backgroundImage: AssetImage('assets/icons/user1.png'),
+                ),
+                Text("User 1")
+              ],
+          ),
+
+
+
+
+      ),
+    );
+  }
+}
