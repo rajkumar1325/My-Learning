0cfc2c5ba150fbb5a7cd5eba8f43903f1b5e3449
Raj kumar <rajkumar.rk0104@gmail.com>
2026-01-28 23:36:56 +0530

    Create Flutter app to display current time formats
    This Flutter application displays the current time in various formats using the intl package. It includes a button to refresh the displayed time.

---
+++ Flutter/50.DateFormatting.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    var currTime = DateTime.now(); //gives current date and time whenever build fn is called
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+
+      body: Column(
+        children: [
+          Text('Current Time is ${DateFormat('hms').format(currTime)}'), //cmd+click on DateFormat to see more available formats
+          Text('Current Time is ${DateFormat('jms').format(currTime)}'),
+          Text('Current Time is ${DateFormat('yMMMMd').format(currTime)}'),
+          ElevatedButton(onPressed: (){
+            setState(() {} );  //setState() --> rebuilds the current app again on every click.
+          }, child:  Text("Click me")),
+          
+
+        ],
+      ),
+
+
+
+    );
+  }
+}
