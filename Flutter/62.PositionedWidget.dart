6dbea8cbed7ef6fdbba0697ef9f870fae63381b1
Raj kumar <rajkumar.rk0104@gmail.com>
2026-02-07 10:59:13 +0530

    Add PositionedWidget.dart for layout demonstration
    Implement a basic Flutter app with a Positioned widget to demonstrate layout capabilities.

---
+++ Flutter/62.PositionedWidget.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
+import 'package:my_project/Widgets/PrimaryBtn.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+
+      body: Container(
+        width: double.infinity, //takes full width
+        height: double.infinity, //takes full height
+        color: Colors.grey,
+        child: Stack(
+          children: [
+
+            // moving the container to the bottom-right side
+            Positioned(
+              bottom: 21,
+              right: 21,
+              child: Container(
+                width: 200,
+                height: 200,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
